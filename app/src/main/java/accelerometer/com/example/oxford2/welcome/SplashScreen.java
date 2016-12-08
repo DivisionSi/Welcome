a61622731df063ebaf0f8b14acdd917d1cd10e2e
DivisionSi <saadbnwhd@gmail.com>
2016-12-08 20:32:34 +0500

    Second commit

--- app/src/main/java/accelerometer/com/example/oxford2/welcome/SplashScreen.java
+++ app/src/main/java/accelerometer/com/example/oxford2/welcome/SplashScreen.java
@@ -14,7 +14,7 @@ import android.content.Intent;
 
 
 public class SplashScreen extends Activity {
-
+//A change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
